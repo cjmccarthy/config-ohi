f51deada8320a49b3109ef866c9965ee56449125
Chris McCarthy <mccart.c.j@gmail.com>
2018-01-25 13:47:05 -0800

    Convert nested leaf nodes to string scalars

--- ingestor/ingestor.go
+++ ingestor/ingestor.go
@@ -3,11 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/clbanning/mxj/x2j"
+	//"github.com/clbanning/mxj/x2j"
+	"github.com/clbanning/mxj"
 	"github.com/ghodss/yaml"
 	"io/ioutil"
-	"os"
-	//"reflect"
+	//	"os"
+	"reflect"
 )
 
 type InfraPayload struct {
@@ -30,48 +31,98 @@ func check(e error) {
 	}
 }
 
+func sanitizeLeaf(leaf interface{}) string {
+	/*
+		fmt.Printf("leaf: %s\n", leaf)
+		fmt.Printf("leaf: %s\n", reflect.TypeOf(leaf))
+			stuff := make([]map[string]interface{}, 2)
+			stuff1 := make(map[string]interface{})
+			stuff2 := make(map[string]interface{})
+			stuff1["qwer"] = "asdf"
+			stuff2["uiop"] = "poui"
+			stuff[0] = stuff1
+			stuff[1] = stuff2
+			fmt.Printf("stuff: %s\n", stuff)
+			fmt.Printf("stuff: %s\n", reflect.TypeOf(stuff))
+	*/
+	marshalled, err := json.Marshal(leaf)
+	check(err)
+	/*
+		fmt.Printf("marshalled: %s\n", marshalled)
+		fmt.Printf("sanit: %s\n", string(marshalled))
+	*/
+	return string(marshalled)
+}
+
+func sanitizeMap(jdata interface{}) interface{} {
+	jmap := reflect.ValueOf(jdata)
+	sanitized := make(map[string]interface{})
+	for _, key := range jmap.MapKeys() {
+		jval := jmap.MapIndex(key)
+		sanitized[key.String()] = sanitizeLeaf(jval.Elem().Interface())
+	}
+	return sanitized
+}
+
+func sanitizeSlice(jdata interface{}) interface{} {
+	jslice := reflect.ValueOf(jdata)
+	sanitized := make([]interface{}, jslice.Len())
+	for i := 0; i < jslice.Len(); i++ {
+		sanitized[i] = sanitizeLeaf(jslice.Index(i).Interface())
+	}
+	return sanitized
+}
+
 func extractConfs(fileDef FileDef) interface{} {
 
 	fdata, err := ioutil.ReadFile(fileDef.Path)
 	check(err)
 
+	var invdata []byte
 	switch ftype := fileDef.Type; ftype {
 	case "yaml":
-		invdata, err := yaml.YAMLToJSON(fdata)
+		invdata, err = yaml.YAMLToJSON(fdata)
 		check(err)
-		var jdata interface{}
-		err = json.Unmarshal(invdata, &jdata)
-		check(err)
-		return jdata
 	case "json":
-		/*
-			Array vs Node top level object?
-
-			rt := reflect.TypeOf(jdata)
-			if rt.Kind() == reflect.Slice {
-
-			}
-		*/
-		var jdata interface{}
-		err = json.Unmarshal(fdata, &jdata)
-		check(err)
-		return jdata
+		invdata = fdata
 	case "xml":
-		invdata, err := x2j.XmlToJson(fdata)
+		/*
+			invdata, err := x2j.XmlToJson(fdata)
+			check(err)
+		*/
+		xdata, err := mxj.NewMapXmlSeq(fdata)
 		check(err)
-		var jdata interface{}
-		err = json.Unmarshal(invdata, &jdata)
-		return jdata
-	case "properties":
+		invdata, err = xdata.Json()
+		check(err)
+	//case "properties":
 	default:
 		fmt.Printf("Unsupported file adapter type %s:", ftype)
 	}
-	return nil
+	var jdata interface{}
+	err = json.Unmarshal(invdata, &jdata)
+	check(err)
+
+	// Encode any leaf maps as scalars to adhere to infra sdk spec
+	// TODO: WTF
+	var sanitized interface{}
+	switch reflect.TypeOf(jdata).Kind() {
+	case reflect.Map:
+		sanitized = sanitizeMap(jdata)
+	case reflect.Slice:
+		sanitized = sanitizeSlice(jdata)
+	default:
+		sanitized = sanitizeLeaf(jdata)
+	}
+
+	return sanitized
 }
 
 func main() {
-	args := os.Args[1:]
-	confData, err := ioutil.ReadFile(args[0])
+	/*
+		args := os.Args[1:]
+		confData, err := ioutil.ReadFile(args[0])
+	*/
+	confData, err := ioutil.ReadFile("./test.yml")
 	check(err)
 
 	confs := []FileDef{}
